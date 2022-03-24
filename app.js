297c0cd3aed77aaa3f3ba16a8da17f9fc369f782
Colin Hoolwerf <choolwerf@gmail.com>
2022-03-24 14:08:39 -0500

    finished adding code snippets from the lecture

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const app = express();
+// const router = express.Router();
 const volleyball = require("volleyball");
 const bodyParser = require("body-parser");
+const { Router } = require("express");
 // const path = require("path");
 
 app.use(volleyball);
 app.use(bodyParser.json());
 // app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 // const staticAssetRoute = express.static(path.join(__dirname, 'public'));
 // app.use(staticAssetRoute);
 
 app.use(express.static("public"));
+
+// router.get("/", (req, res, next) => {
+//   res.send(SEND THINGS HERE)
+// });
 
 app.get("/", (req, res, next) => {
   res.send("Hello World!")
