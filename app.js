bc2cecd4d8f955afddfad716dbf40ecca44cf45a
Colin Hoolwerf <choolwerf@gmail.com>
2022-03-24 16:07:53 -0500

    Completed up to 'Add a Single-Post Route' and changed the titles to a tags

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 const express = require("express");
-const { Router } = require("express");
-const router = express.Router();
 const app = express();
-const path = require("path");
 const volleyball = require("volleyball");
 const postBank = require("./postBank");
 
-app.use(volleyball);
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// const staticAssetRoute = express.static(path.join(__dirname, 'public'));
-// app.use(staticAssetRoute);
-
-app.use(express.static("public"));
-
-// router.get("/", (req, res, next) => {
-//   res.send(SEND THINGS HERE)
-// });
+app.use(volleyball);
 
 app.get("/", (req, res, next) => {
   const posts = postBank.list()
+  const html = 
+  `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span><a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`
+  res.send(html)
+});
+
+// app.get( '/posts/:id', (req, res) => {
+//   console.log( req.params.id );
+// });
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
   const html = 
   `<!DOCTYPE html>
   <html>
@@ -45,7 +68,7 @@ app.get("/", (req, res, next) => {
     </div>
   </body>
 </html>`
-  res.send(html)
+  res.send(html);
 });
 
 const PORT = 1337;
