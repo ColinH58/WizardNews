2d277c86786f3f3c45065e3a94a0d18acfeb3a2e
Colin Hoolwerf <choolwerf@gmail.com>
2022-03-24 10:22:14 -0500

    formatted code a bit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 const volleyball = require("volleyball");
+app.use(volleyball);
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(volleyball);
 
 app.get("/", (req, res, next) => {
   res.send("Hello World!")
