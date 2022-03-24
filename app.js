56c9203b62f6842bfca501b0b21374cbc11d528a
Colin Hoolwerf <choolwerf@gmail.com>
2022-03-24 10:19:32 -0500

    added more middleware and deleted example

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-//WIZARD NEWS
 const express = require("express");
-const volleyball = require("volleyball");
 const app = express();
-app.use(volleyball);
+const bodyParser = require('body-parser');
+const volleyball = require("volleyball");
 
 app.use(express.static('public'));
+app.use(bodyParser);
+app.use(volleyball);
 
 app.get("/", (req, res, next) => {
   res.send("Hello World!")
