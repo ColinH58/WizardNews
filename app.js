dee9e2cee313c64f95d3601e8330b7abee573ed2
Colin Hoolwerf <choolwerf@gmail.com>
2022-03-24 12:26:34 -0500

    added static and path from lecture

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
+const volleyball = require("volleyball");
+// const path = require("path");
+const bodyParser = require("body-parser");
 
-const bodyParser = require('body-parser');
+app.use(volleyball);
 app.use(bodyParser.json());
 
-const volleyball = require("volleyball");
-app.use(volleyball);
+// const staticAssetRoute = express.static(path.join(__dirname, 'public'));
+// app.use(staticAssetRoute);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
   res.send("Hello World!")
