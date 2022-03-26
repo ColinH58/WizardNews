7a459aa7a61eb27ba49db9a11596390763ac01f3
Colin Hoolwerf <choolwerf@gmail.com>
2022-03-26 11:08:04 -0500

    project completed with all bonus steps (except the HTML tag on PostList)

--- views/errorMessage.js
+++ views/errorMessage.js
@@ -1,22 +1,20 @@
-const errorMessage = (err) => {
-  const html = `<!DOCTYPE html>
-    <html>
-    <head>
-      <title>Wizard News</title>
-      <link rel="stylesheet" href="/style.css" />
-    </head>
-    <body>
-      <div class="news-list">
-        <header><img src="/logo.png"/>Wizard News</header>
-          <div class='news-item'>
-            <p>Page Not Found!</p>
-            <br>
-            <a href="/">Click Here to Go Home</a>
-          </div>
-      </div>
-    </body>
-  </html>`;
-  return html;
-};
+const html = require("html-template-tag");
 
-module.exports = errorMessage;
+module.exports = errorMessage = (err) => 
+html`<!DOCTYPE html>
+<html>
+<head>
+  <title>Wizard News</title>
+  <link rel="stylesheet" href="/style.css" />
+</head>
+<body>
+  <div class="news-list">
+    <header><img src="/logo.png"/>Wizard News</header>
+      <div class='news-item'>
+        <p>Page Not Found!</p>
+        <br>
+        <a href="/">Click Here to Go Home</a>
+      </div>
+  </div>
+</body>
+</html>`;
--- views/postDetails.js
+++ views/postDetails.js
@@ -1,27 +1,25 @@
-const postDetails = (post) => {
-  const html = `<!DOCTYPE html>
-    <html>
-    <head>
-      <title>Wizard News</title>
-      <link rel="stylesheet" href="/style.css" />
-    </head>
-    <body>
-      <div class="news-list">
-        <header><img src="/logo.png"/>Wizard News</header>
-          <div class='news-item'>
-            <p>
-              <span class="news-position">${post.id}. ▲</span><a href="/">${post.title}</a>
-              <small>(by ${post.name})</small>
-            </p>
-            <small class="news-info">
-              ${post.upvotes} upvotes | ${post.date}
-            </small>
-            <p>${post.content}</p>
-          </div>
-      </div>
-    </body>
-  </html>`;
-  return html;
-};
+const html = require("html-template-tag");
 
-module.exports = postDetails;
+module.exports = postDetails = (post) => 
+html`<!DOCTYPE html>
+<html>
+<head>
+  <title>Wizard News</title>
+  <link rel="stylesheet" href="/style.css" />
+</head>
+<body>
+  <div class="news-list">
+    <header><img src="/logo.png"/>Wizard News</header>
+      <div class='news-item'>
+        <p>
+          <span class="news-position">${post.id}. ▲</span><a href="/">${post.title}</a>
+          <small>(by ${post.name})</small>
+        </p>
+        <small class="news-info">
+          ${post.upvotes} upvotes | ${post.date}
+        </small>
+        <p>${post.content}</p>
+      </div>
+  </div>
+</body>
+</html>`;
